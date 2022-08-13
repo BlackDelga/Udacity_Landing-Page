bceac5d748254299c588547560c8052bd620e290
BlackDelga <evanbdelgado@gmail.com>
2022-08-12 22:53:14 -0400

    recode
    update all code

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,56 +1,27 @@
-/*
- Define Global Variables
- */
-const sections = Array.from(document.querySelectorAll("section"));
-const menu = document.getElementById("navbar__list");
-let numberOfListItems = sections.length;
+window.onload = () => {
+	const tab_switchers = document.querySelectorAll("[data-switcher]");
 
-/*
-End Global Variables
-Start Helper Functions
- */
-function createListItem() {
-	for (section of sections) {
-		sectioName = section.getAttribute("data-nav");
-		sectionLink = section.getAttribute("id");
-		//create an item for each one
-		listItem = document.createElement("li");
+	for (let i = 0; i < tab_switchers.length; i++) {
+		const tab_switcher = tab_switchers[i];
+		const page_id = tab_switcher.dataset.tab;
 
-		// add the item text here
-		listItem.innerHTML = `<a class='menu__link' href='#${sectionLink}'>${sectioName}</a>`;
+		tab_switcher.addEventListener("click", () => {
+			document
+				.querySelector(".tabs .tab.is-active")
+				.classList.remove("is-active");
+			tab_switcher.parentNode.classList.add("is-active");
 
-		// add listItem to the menu here
-		menu.appendChild(listItem);
+			SwitchPage(page_id);
+		});
 	}
+};
+
+function SwitchPage(page_id) {
+	const current_page = document.querySelector(".pages .page.is-active");
+	current_page.classList.remove("is-active");
+
+	const next_page = document.querySelector(
+		`.pages .page[data-page="${page_id}"]`
+	);
+	next_page.classList.add("is-active");
 }
-
-//Determines if sections is in the viewport
-function sectionInViewPort(elem) {
-	let sectionPos = elem.getBoundingClientRect();
-	return sectionPos.top >= 0;
-}
-
-//if the section being viewed give a different style (appearance)
-function toggleActiveClass() {
-	for (section of sections) {
-		//if the section is in the viewport
-		if (sectionInViewPort(section)) {
-			//check if it doesn't already contain "your-active-class"
-			if (!section.classList.contains("your-active-class")) {
-				//then add it
-				section.classList.add("your-active-class");
-			}
-		} else {
-			//if it's not there then remove "your-active-class"
-			section.classList.remove("your-active-class");
-		}
-	}
-}
-
-// End Helper Functions
-
-// build the nav
-createListItem();
-
-// Add class 'active' to section when near top of viewport
-document.addEventListener("scroll", toggleActiveClass);
